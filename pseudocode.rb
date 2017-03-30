67173332253c6493a06d86ad421d17498d405ded
jpileto <jp.ileto@icloud.com>
2017-03-29 21:04:25 -0400

    Added the solution to the last problem for the coffee & code session 1

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,7 +1,19 @@
-for num in 1..10
+for num in 1..100
   if num % 2 == 0
     puts "even"
   else
     puts "odd"
   end
 end
+
+
+# Write a program that prints the numbers from 1 to 100.
+#
+# For the multiples of 3 print "Bitmaker" instead of the number.
+# For the multiples of 5 print "HQ" instead of the number.
+# For numbers which are multiples of both 3 and 5 print "BitmakerHQ"
+
+# For the problem above here are my steps to arrive to a solution:
+# a. use the for loop so I can loop all the numbers from 1 through 100
+# b. formulate an if/else statement to check on the 3 conditions listed above
+# and print the correct return value for each number being evaluated.
